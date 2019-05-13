dab262c8075f8ea63c0072b5e2377d6b4d1ba277
marcelbuesing <buesing.marcel@googlemail.com>
2019-05-13 13:56:24 +0200

    Add regression test for #505

---
+++ tests/regression/issue505.rs
@@ -0,0 +1,46 @@
+use serde_derive::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct SI32 {
+    value: i32
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+struct SU128 {
+    value: u128
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[serde(tag = "type")]
+enum E {
+    SI32(SI32),
+    SU128(SU128),
+}
+
+#[test]
+#[cfg(feature = "arbitrary_precision")]
+fn test_i32() {
+    let s = SI32 { value: std::i32::MAX };
+    let e = E::SI32(s);
+    let v = serde_json::to_string(&e).unwrap();
+    let e = serde_json::from_str::<E>(&v).unwrap();
+
+    match e {
+        E::SI32(s) => assert_eq!(s, SI32 { value: std::i32::MAX }),
+        _ => unreachable!(),
+    }
+}
+
+#[test]
+#[cfg(feature = "arbitrary_precision")]
+fn test_u128() {
+    let s = SU128 { value: std::u128::MAX };
+    let e = E::SU128(s);
+    let v = serde_json::to_string(&e).unwrap();
+    let e = serde_json::from_str::<E>(&v).unwrap();
+
+    match e {
+        E::SU128(s) => assert_eq!(s, SU128 { value: std::u128::MAX }),
+        _ => unreachable!(),
+    }
+}
